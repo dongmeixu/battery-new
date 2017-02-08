2426b5b81b9bf2e619e343f13b42ca0dde99cc4f
yangpanRoy <619821473@qq.com>
2017-02-08 19:47:25 +0800

    实现从json-server获取数据，增加出入库界面物流信息展示，进行了美化工作
    移除JsonLoader工具类，放弃从本地assets文件夹获取json数据；新建Bean和Callback两个package包，使用第三方封装的okhttputils从局域网获取json-server数据（okhttputils：https://github.com/hongyangAndroid/okhttputils），使用Gson解析json；
    在出入库交易界面，POST
    json字符串到json-server并拉取，更新RecycleView列表，达到展示交易记录的目的。翻转了交易信息列表，使得最近一次的交易记录会加到列表最上面；
    
    调整主界面和搜索结果界面Item布局，增加了左右两侧边距，调整文字布局，使得图片可以更好显示。更改了主界面banner布局，缩小banner宽度留出边距，增加撞色背景，用CardView修剪banner轮播图并设置圆阴影阴影。
    TODO：根据出库/入库的radioButton选择来记录交易记录，需要重写POST方法；美化工作。

--- android/app/src/main/java/layout/fragment_deal.java
+++ android/app/src/main/java/layout/fragment_deal.java
@@ -10,6 +10,8 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,10 +23,22 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.novadata.batteryapp.MainActivity;
 import com.novadata.batteryapp.R;
 import com.novadata.batteryapp.ScanActivity;
+import com.zhy.http.okhttp.OkHttpUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import Bean.Import_Export_Item;
+import Callback.ListImportExportItemCallback;
+import Callback.MyStringCallback;
+import adapter.ImportExportItemAdapter;
+import okhttp3.Call;
+import okhttp3.MediaType;
 import utils.PhotoSaver;
 
 public class fragment_deal extends Fragment implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
@@ -38,6 +52,10 @@ public class fragment_deal extends Fragment implements View.OnClickListener, Rad
     private RadioGroup rg;
     private Bitmap bitmap;
     private EditText carEt;
+    private RecyclerView rv;
+    private ImportExportItemAdapter ieItemAdapter;
+    private ArrayList<HashMap<String,Object>> listItem = new ArrayList<HashMap<String,Object>>();
+    private Import_Export_Item latestImportExportItem;
 
     int login_status = -1;
     int status_IO;
@@ -68,6 +86,9 @@ public class fragment_deal extends Fragment implements View.OnClickListener, Rad
         photo_Iv = (ImageView) view.findViewById(R.id.photo);
         carEt = (EditText) view.findViewById(R.id.car_editText);
 
+        photo_Tv.setVisibility(View.VISIBLE);
+        confirm_Tv.setVisibility(View.GONE);
+
         photo_Tv.setOnClickListener(this);
         confirm_Tv.setOnClickListener(this);
     }
@@ -75,6 +96,8 @@ public class fragment_deal extends Fragment implements View.OnClickListener, Rad
     private void initCompanyView() {
         start2Scan_Tv = (TextView) view.findViewById(R.id.scan_button);
         rg = (RadioGroup) view.findViewById(R.id.company_RadioGroup);
+
+        getList();//GET整个出入库扫描信息
 
         start2Scan_Tv.setOnClickListener(this);
         rg.setOnCheckedChangeListener(this);
@@ -158,7 +181,11 @@ public class fragment_deal extends Fragment implements View.OnClickListener, Rad
                         String photoName = PhotoSaver.createPhotoName();
                         PhotoSaver.savePhoto2SDCard(PATH, photoName, bitmap);
                         Toast.makeText(getActivity(), "照片存储成功！路径为 " + PATH, Toast.LENGTH_SHORT).show();
+                        photo_Tv.setVisibility(View.VISIBLE);
+                        confirm_Tv.setVisibility(View.GONE);
+                        photo_Iv.setImageDrawable(getResources().getDrawable(R.drawable.contract));
                         //TODO 将车架号和合同照片共同上传
+
                     }
                     else {
                         Toast.makeText(getActivity(), "请拍摄合同照片后再确认提交", Toast.LENGTH_SHORT).show();
@@ -196,11 +223,20 @@ public class fragment_deal extends Fragment implements View.OnClickListener, Rad
 
             if (requestCode == REQUEST_SCAN){
                 String result= bundle.getString("result");//获得扫描的二维码信息
+                if ((latestImportExportItem == null) && (status_IO == IMPORT))
+                {
+                    Toast.makeText(getActivity(), "ERROR：未找到出库信息，无法完成入库！", Toast.LENGTH_SHORT).show();
+                }//TODO 条件判断完善
                 //TODO 将结果展示
+                initList();
             }
             if (requestCode == REQUEST_PHOTO){
                 bitmap=(Bitmap) bundle.get("data");//从附加值中获取返回的图像
                 photo_Iv.setImageBitmap(bitmap);//显示缩略图像
+                if (photo_Iv != null){
+                    photo_Tv.setVisibility(View.GONE);
+                    confirm_Tv.setVisibility(View.VISIBLE);
+                }
             }
 
         } if(resultCode == getActivity().RESULT_CANCELED) {
@@ -208,5 +244,73 @@ public class fragment_deal extends Fragment implements View.OnClickListener, Rad
         }
     }
 
+    private void initList() {
+        //先POST到json server
+        OkHttpUtils
+                .postString()
+                .url("http://192.168.191.1:3000/Import_Export_Item")
+                .mediaType(MediaType.parse("application/json; charset=utf-8"))
+                .content(new Gson().toJson(new Import_Export_Item("模组：11A2FMZABCDEF1212345AB...", "深圳比克电池厂", "深圳比克汽车厂")))
+                /*TODO 此处应根据出入库radioButton的响应事件来决定是出库还是入库
+                       根据出入库的状态决定当前账户的身份应作为 出发地 还是 目的地*/
+                .build()
+                .execute(new MyStringCallback());
+        //再GET整个出入库扫描信息
+        getList();
+    }
+
+
+    public void getList() {
+        OkHttpUtils
+                .get()//
+                .url("http://192.168.191.1:3000/Import_Export_Item")//
+                .build()//
+                .execute(new ListImportExportItemCallback()//
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.i("Tag", "ListImportExportItemCallback Error");
+                    }
+
+                    @Override
+                    public void onResponse(List<Import_Export_Item> response, int id) {
+                        if (response.size() > 0) {
+                            listItem.removeAll(listItem);
+                            for (int i = 0; i < response.size(); i++) {
+                                HashMap<String, Object> map = new HashMap<>();
+                                map.put("item_module_id", response.get(i).getItem_module_id());
+                                map.put("logistics_source", response.get(i).getLogistics_source());
+                                map.put("logistics_destination", response.get(i).getLogistics_destination());
+                                listItem.add(map);
+                                initView();
+                            }
+                            Log.i("Tag", "ListImportExportItemCallback Success");
+                            latestImportExportItem = response.get(response.size() - 1);
+                        } else {
+                            Log.i("Tag", "ListImportExportItemCallback Empty");
+                        }
+                    }
+                });
+    }
+
+    public void initView(){
+        //为ListView绑定适配器
+        ieItemAdapter = new ImportExportItemAdapter(getActivity(), listItem);
+
+        rv = (RecyclerView) view.findViewById(R.id.import_export_recycleView);
+        rv.setAdapter(ieItemAdapter);
+        //使用线性布局
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setStackFromEnd(true);//列表再底部开始展示，反转后由上面开始展示
+        layoutManager.setReverseLayout(true);//列表翻转
+        rv.setLayoutManager(layoutManager);
+        rv.setHasFixedSize(true);
+        //Rv.addItemDecoration(new DividerItemDecoration(getActivity(), layoutManager.getOrientation()));//用类设置分割线
+        //Rv.addItemDecoration(new DividerItemDecoration(this, R.drawable.list_divider)); //用已有图片设置分割线
+
+        //设置Item之间的间距
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.item_space);
+        rv.addItemDecoration(new SpaceItemDecoration(spacingInPixels));
+    }
 
 }
--- android/app/src/main/java/layout/fragment_main.java
+++ android/app/src/main/java/layout/fragment_main.java
@@ -5,33 +5,39 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
 import com.novadata.batteryapp.R;
 import com.youth.banner.Banner;
+import com.zhy.http.okhttp.OkHttpUtils;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import Bean.Main_Search_History_Item;
+import Callback.ListBannerImageUrlCallback;
+import Callback.ListMainSearchHistoryItemCallback;
 import adapter.MyItemClickListener;
 import adapter.SearchHistoryItemAdapter;
-import utils.JsonLoader;
+import okhttp3.Call;
 
 public class fragment_main extends Fragment implements MyItemClickListener{
 
     private View view;
     private Banner banner;
 
-    private RecyclerView Rv;
     //设置Item内组件资源
-    private ArrayList<HashMap<String,Object>> listItem = new ArrayList<HashMap<String,Object>>();
-    private SearchHistoryItemAdapter shItemAdapter;
+    private ArrayList<HashMap<String,Object>> listItem = new ArrayList<>();
 
     //设置图片资源:url或本地资源
-    List<String> Banner_image_url = new ArrayList<String>();
+    List<String> Banner_image_url = new ArrayList<>();
 
     @Nullable
     @Override
@@ -41,18 +47,106 @@ public class fragment_main extends Fragment implements MyItemClickListener{
 
         view = inflater.inflate(R.layout.fragment_main, container, false);
         banner = (Banner) view.findViewById(R.id.banner);
-        //读取json数据
-        JsonLoader jsonLoader = new JsonLoader("db.json");
-        Banner_image_url = jsonLoader.loadJson2container("banner_image_url", Banner_image_url);
-        listItem = jsonLoader.loadJson2container("search_history_item", listItem);
-        //设置banner广告图样式
-        initItemHead();
-        initView();
+
+        initBanner();
+        initList();
 
         return view;
     }
 
-    private void initItemHead() {
+    private void initList() {
+        OkHttpUtils
+                .get()//
+                .url("http://192.168.191.1:3000/search_history_item")//
+                .build()//
+                .execute(new ListMainSearchHistoryItemCallback()//
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.i("Tag", "ListMainSearchHistoryItemCallback Error");
+                    }
+
+                    @Override
+                    public void onResponse(List<Main_Search_History_Item> response, int id) {
+                        if (response.size() > 0) {
+                            listItem.removeAll(listItem);
+                            for (int i = 0; i < response.size(); i++) {
+                                HashMap<String, Object> map = new HashMap<>();
+                                map.put("ItemTitle", response.get(i).getTitle());
+                                map.put("ItemText1", "编号：" + response.get(i).getModule_num());
+                                map.put("ItemText2", "生产日期：" + response.get(i).getProduce_date());
+                                map.put("ItemText3", "生产企业：" + response.get(i).getProducer());
+                                map.put("ItemText4", "最近流通时间：" + response.get(i).getLatest_logistics_date());
+                                map.put("ItemText5", "最近流通地点：" + response.get(i).getLatest_logistics_place());
+                                map.put("ItemImage", response.get(i).getModule_image());
+                                listItem.add(map);
+                                initView();
+                            }
+                            Log.i("Tag", "ListMainSearchHistoryItemCallback Success");
+
+                        } else {
+                            Log.i("Tag", "ListMainSearchHistoryItemCallback Empty");
+                        }
+                    }
+                });
+    }
+
+    public void initView(){
+        //为ListView绑定适配器
+        SearchHistoryItemAdapter shItemAdapter = new SearchHistoryItemAdapter(getActivity(), listItem);
+        shItemAdapter.setOnItemClickListener(this);
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        rv.setAdapter(shItemAdapter);
+        //使用线性布局
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(layoutManager);
+        rv.setHasFixedSize(true);
+        //Rv.addItemDecoration(new DividerItemDecoration(getActivity(), layoutManager.getOrientation()));//用类设置分割线
+        //Rv.addItemDecoration(new DividerItemDecoration(this, R.drawable.list_divider)); //用已有图片设置分割线
+
+        //设置Item之间的间距
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.item_space);
+        rv.addItemDecoration(new SpaceItemDecoration(spacingInPixels));
+    }
+
+    private void initBanner() {
+        OkHttpUtils
+                .get()//
+                .url("http://192.168.191.1:3000/banner_image_url")//
+                .build()//
+                .execute(new ListBannerImageUrlCallback()//
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id)
+                    {
+                        Log.i("Tag", "ListBannerImageUrlCallback Error");
+                    }
+
+                    @Override
+                    public void onResponse(List<String> response, int id)
+                    {
+                        if (response.size() > 0)
+                        {
+                            Banner_image_url.removeAll(Banner_image_url);
+                            for (int i = 0; i < response.size(); i++)
+                            {
+                                Banner_image_url.add(response.get(i));
+                                //设置banner广告图样式
+                                initBannerStyle();
+                            }
+                            Log.i("Tag", "ListBannerImageUrlCallback Success");
+
+                        }else
+                        {
+                            Log.i("Tag", "ListBannerImageUrlCallback Empty");
+                        }
+
+                    }
+                });
+    }
+
+    private void initBannerStyle() {
         //设置样式,默认为:Banner.NOT_INDICATOR(不显示指示器和标题)
         //可选样式如下:
         //1. Banner.CIRCLE_INDICATOR    显示圆形指示器
@@ -99,30 +193,11 @@ public class fragment_main extends Fragment implements MyItemClickListener{
         });
     }
 
-    public void initView(){
-        //为ListView绑定适配器
-        shItemAdapter = new SearchHistoryItemAdapter(getActivity(),listItem);
-        shItemAdapter.setOnItemClickListener(this);
-
-        Rv = (RecyclerView) view.findViewById(R.id.my_recycler_view);
-        Rv.setAdapter(shItemAdapter);
-        //使用线性布局
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        Rv.setLayoutManager(layoutManager);
-        Rv.setHasFixedSize(true);
-        //Rv.addItemDecoration(new DividerItemDecoration(getActivity(), layoutManager.getOrientation()));//用类设置分割线
-        //Rv.addItemDecoration(new DividerItemDecoration(this, R.drawable.list_divider)); //用已有图片设置分割线
-
-        //设置Item之间的间距
-        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.item_space);
-        Rv.addItemDecoration(new SpaceItemDecoration(spacingInPixels));
-    }
-
     @Override
-    public void onItemClick(View view, int postion) {//点击事件的回调函数
+    public void onItemClick(View view, int position) {//点击事件的回调函数
         //TODO 定义Item的点击响应事件
-        System.out.println("点击了第" + postion + "行");
-        Toast.makeText(getActivity(), "点击了第"+postion+"行模组信息", Toast.LENGTH_SHORT).show();
+        System.out.println("点击了第" + position + "行");
+        Toast.makeText(getActivity(), "点击了第"+position+"行模组信息", Toast.LENGTH_SHORT).show();
     }
 
 }
