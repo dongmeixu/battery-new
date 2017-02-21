1a2c0c0c02b21ddf47ef09ac0596e7f9cdcfc6ad
yinxinyu <mtgqyxy@163.com>
2017-02-21 21:44:54 +0800

    完成了证书申请状态查询界面，二维码生成界面以及汽车厂的界面

---
+++ frontend-frame/src/app/pages/car/carCtrl.js
@@ -0,0 +1,78 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.car')
+      .controller('carCtrl', carCtrl);
+
+  /** @ngInject */
+  function carCtrl($scope, $filter, editableOptions, editableThemes) {
+
+/*    $scope.smartTablePageSize = 10;
+
+    $scope.smartTableData = [
+      {
+        id: 1,
+        firstName: '2016.12.12',
+        lastName: '1000000',
+        username: '比克电池',
+        email: '11A2FMZABCDEF1212345AB...',
+        age: '宏远4s店'
+      },
+     
+    ];
+
+    $scope.statuses = [
+      {value: 1, text: 'Good'},
+      {value: 2, text: 'Awesome'},
+      {value: 3, text: 'Excellent'},
+    ];
+
+    $scope.groups = [
+      {id: 1, text: 'user'},
+      {id: 2, text: 'customer'},
+      {id: 3, text: 'vip'},
+      {id: 4, text: 'admin'}
+    ];
+
+    $scope.showGroup = function(user) {
+      if(user.group && $scope.groups.length) {
+        var selected = $filter('filter')($scope.groups, {id: user.group});
+        return selected.length ? selected[0].text : 'Not set';
+      } else return 'Not set'
+    };
+
+    $scope.showStatus = function(user) {
+      var selected = [];
+      if(user.status) {
+        selected = $filter('filter')($scope.statuses, {value: user.status});
+      }
+      return selected.length ? selected[0].text : 'Not set';
+    };
+
+
+    $scope.removeUser = function(index) {
+      $scope.users.splice(index, 1);
+    };
+
+    $scope.addUser = function() {
+      $scope.inserted = {
+        id: $scope.users.length+1,
+        name: '',
+        status: null,
+        group: null
+      };
+      $scope.users.push($scope.inserted);
+    };
+
+    editableOptions.theme = 'bs3';
+    editableThemes['bs3'].submitTpl = '<button type="submit" class="btn btn-primary btn-with-icon"><i class="ion-checkmark-round"></i></button>';
+    editableThemes['bs3'].cancelTpl = '<button type="button" ng-click="$form.$cancel()" class="btn btn-default btn-with-icon"><i class="ion-close-round"></i></button>';
+
+*/
+  }
+
+})();
--- frontend-frame/src/app/pages/enterprise/myApply/myApply.js
+++ frontend-frame/src/app/pages/enterprise/myApply/myApply.js
@@ -1,40 +1,41 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('BlurAdmin.pages.enterprise')
-      .controller('myApplyCtrl', myApplyCtrl);
+    angular.module('BlurAdmin.pages.enterprise')
+        .controller('myApplyCtrl', myApplyCtrl);
 
-  /** @ngInject */
-  function myApplyCtrl($scope, $http, toastr) {
-    var applyApi = 'http://localhost:3030/certs';
+    /** @ngInject */
+    function myApplyCtrl($scope, $http, toastr) {
+        var applyApi = 'http://localhost:3030/v1/company/';
 
-    //默认值
-    $scope.applyData = {
-      'companyType': '1',
-      'companyName': '某某电池厂',
-      'creditCode': '',
-      'factoryCode': '',
-      'contact': '',
-      'phone': '',
-      'email': '',
-      'certFile': ''
-    };
+        //默认值
+        $scope.show = "";
+        $scope.status = 0;
+        $scope.id = "";
 
-    //确认录入
-    $scope.confirm = function() {
-      $http.post(applyApi, $scope.applyData)
-        .success(function(data){
-          toastr.success('录入成功', '', {
-            "timeOut": "1000",
-            "closeButton": false,
-          });
-        })
-        .error(function(data){
-          toastr.error('录入失败', '', {});
-          console.log("error: ", data);
-        });
+        switch ($scope.status) {
+            case 0:
+                $scope.show = "未审核";
+                break;
+            case 1:
+                $scope.show = "审核通过";
+                break;
+            case 2:
+                $scope.show = "审核不通过";
+                break;
+        }
+
+
+        //确认录入
+        $http.get(applyApi + $scope.id + '/' + 'certs')
+            .success(function (data) {
+                $scope.status = data.status;
+            })
+            .error(function (data) {
+                toastr.error('失败', '', {});
+                console.log("error: ", data);
+            });
+
     }
-  }
-
 })();
 
